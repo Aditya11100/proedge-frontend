3acaf41f02b403a7636890cfa3e8ef2851d761cb
Aditya11100 <a4adityaraj@gmail.com>
2021-06-06 00:09:37 +0530

    frontend working

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className="card-panel black">
+            <center>
+                <h1 style={{ color: "white" }}>PROEDGE ASSIGNMENT</h1>
+            </center>
+        </header>
+    );
+}
+
+export default Header;
